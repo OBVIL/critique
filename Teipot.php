dbfd40840852e0ce9abeb92d3f5a366907288304
Frédéric Glorieux <glorieux-f@users.noreply.github.com>
2013-06-17 09:21:41 +0000

    Nouveau modèle SQLite

---
+++ Teipot.php
@@ -0,0 +1,203 @@
+<?php
+
+
+/** 
+ Deals with an sqlite base produced by Teipub.php
+ Make it dynamic, we can imagine to have multiple bases to link on
+ */
+class Teipot {
+  /** sqlite File  */
+  private $sqliteFile;
+  /** sqlite link */
+  public $pdo;
+  /** requested path */
+  public $path;
+  /** ../ */
+  public $baseHref;
+  /** Content-Type header */
+  static $mime=array(
+      "css"  => 'text/css; charset=UTF-8',
+      "epub" => 'application/epub+zip',
+      "html" => 'text/html; charset=UTF-8',
+      "jpg"  => 'image/jpeg',
+      "png"  => 'image/png',
+      "xml"  => 'text/xml',
+    );
+  /** Generated messages */
+  private static $msg=array(
+    "authors"=>array("en"=>"Author(s)","fr"=>"Auteur(s)"),
+    "date"=>array("en"=>"Date","fr"=>"Date"),
+    "title"=>array("en"=>"Title","fr"=>"Titre"),
+  );
+  /** Default lang for messages */
+  public $lang="en";
+  
+  /** Constructor, link on the base */
+  public function __construct($sqliteFile, $lang="en", $path="") {
+    if (!$path && isset($_SERVER['PATH_INFO'])) {
+      $this->path=ltrim($_SERVER['PATH_INFO'], '/');
+    }
+    $this->baseHref=str_repeat("../", substr_count($this->path, '/'));
+    if ($lang) {
+      $this->lang=$lang;
+      setlocale(LC_ALL, $lang.'.UTF-8');
+    }
+    $this->sqliteFile = $sqliteFile;
+    $this->pdo=new PDO("sqlite:".$sqliteFile);
+    $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+  }
+  /** Display a message */
+  public function msg($key, $lang=false, $arg=array()) {
+    if(!$lang) $lang=$this->lang;
+    if (isset(self::$msg[$key]) && isset(self::$msg[$key][$lang])) $key=self::$msg[$key][$lang];
+    if (!count($arg)) echo $key;
+    else if (count($arg) > 2 ) printf($key, $arg[0], $arg[1], $arg[2]);
+    else if (count($arg) > 1 ) printf($key, $arg[0], $arg[1]);
+    else if (count($arg) > 0 ) printf($key, $arg[0]);
+  }
+  /** serve a static file */
+  public function get($href=null) {
+    if(!$href) $href=$this->path;
+    if (!$href) return false;
+    $query=$this->pdo->prepare('SELECT type, cont FROM file WHERE href = ? ') ;
+    $query->execute(array($href));
+    list($type, $cont)=$query->fetch();
+    if (!$cont) return false;
+    if (!$type) {
+      $ext=pathinfo($dest, PATHINFO_EXTENSION);
+      if (isset($mime[$ext])) $type=$mime[$ext];
+    }
+    if ($type) header("Content-Type: ".$type);
+    echo $cont;
+    exit;
+  }
+  /** Output table of all books */
+  public function bib() {
+    $query=$this->pdo->prepare('SELECT href, name FROM file WHERE book = ? ') ;
+    echo '
+  <table class="sortable">
+    <tr><th>',$this->msg('authors'),'</th><th>',$this->msg('date'),'</th><th>',$this->msg('title'),'</th></tr>  
+    ';
+    $sql =  'SELECT id, name, byline, created, title FROM book ORDER BY byline, created';
+    
+    foreach  ($this->pdo->query($sql) as $row) {
+      // '<th>','<a href="',$this->baseHref, $row['name'],'/">',$row['name'],'</a></th>',
+      echo "\n<tr>",'<td>',$row['byline'],'</td>','<td>',$row['created'],'</td>','<td><a href="',$this->baseHref, $row['name'],'/">',$row['title'],'</a></td>';
+      $sep="\n<td>";
+      $query->execute(array($row['id']));
+      while($link=$query->fetch()) {
+        echo $sep,'<a href="',$this->baseHref,$link['href'],'">',$link['name'],'</a>';
+        $sep=", ";
+      }
+      if ($sep==", ")echo"</td>";
+      echo "</tr>";
+    }
+    echo "</table>\n";
+  }
+  
+  
+  /** rewrite HTML links to add query links */
+  static function reHref($html, $q=null) {
+    if(!$q && !isset($_REQUEST['q'])) return $html;
+    if(!$q) $q=$_REQUEST['q'];
+    return preg_replace('@ href="([^"]+)"@', ' href="$1?q='.$q.'#mark1"', $html);
+  }
+
+  /** Résultats de recherche */
+  static function q($q, $bookId=null) {
+    $docMax=100;
+    // recherche juste dans un livre
+    if(isset($bookId)) {
+      $query=$this->pdo->prepare('SELECT COUNT(*) AS rowcount FROM search, article WHERE article.book=? AND search.rowid=article.rowid AND search.text MATCH ?');
+      $query->execute(array($bookId, $q));
+      list($rowcount)=$query->fetch();
+      $query=$this->pdo->prepare('SELECT article.breadcrumb, article.name, article.href, search.text, offsets(search) AS offsets FROM search, article WHERE article.book=? AND search.rowid=article.rowid AND search.text MATCH ? LIMIT ?; ') ;
+      $query->execute(array($bookId, $q, $docmax));
+    }
+    else {
+      $query=$this->pdo->prepare('SELECT COUNT(*) AS rowcount FROM search WHERE text MATCH ? ') ;
+      $query->execute(array($q));
+      list($rowcount)=$query->fetch();
+      // on croise la table des articles avec la table full-text, la table full-text n'indexe pas certaines colonnes utiles à des tris (date, auteur)
+      $query=$this->pdo->prepare('SELECT article.breadcrumb, article.name, article.href, search.text, offsets(search) AS offsets FROM search, article WHERE text MATCH ? AND search.rowid=article.rowid LIMIT ?; ') ;
+      $query->execute(array($q, $docMax));
+    }
+    $docnum=1;
+    echo "\n",'<table width="100%" class="search">';
+    if(!$rowcount) echo "\n",'<caption>Pas de résultats pour la recherche : ',$q,'</caption>';
+    // TODO, join quote expressions 
+    else { 
+      if($rowcount > $docMax) echo "\n",'<caption>',$rowcount,' textes trouvés, affichage limité à ',$docMax,'. Votre recherche : ',$q,'</caption>';
+      else echo "\n",'<caption>',$rowcount,' textes trouvés. Votre recherche : ',$q,'</caption>';
+      while ($row=$query->fetch()) {
+        $offsets=explode(' ',$row['offsets']);
+        $count=count($offsets);
+        $occ=floor($count/4);
+        $more="";
+        $occDisp=$occ;
+        $occMax=50;
+        if ($occ > $occMax) {
+          $more='<tr><td/><td colspan="3" align="right">Plus de '.$occMax.' occurrences dans ce texte ('.$occ.'), vous les retrouvez toutes surlignées dans le <a href="'.$pot->baseHref.$row['href'].'?q='.$q.'#mark1">texte complet</a></td></tr>';
+          $count=$occMax*4;
+          $occDisp=$occMax.'/'.$occ;
+        }
+        if($pot->baseHref=='') $row['breadcrumb']=preg_replace('@"../@', '"', $row['breadcrumb']);
+        echo "\n",'<tr><th class="num">',$docnum,'</th><th colspan="3">',Teipot::reHref($row['breadcrumb']),' (',$occDisp,' occ.)</td></tr>';
+        // echo '<tr><td colspan="3"><pre style="white-space:pre-wrap; font-family:serif; ">',$row['text'],'</pre></td></tr>';
+        $mark=1;
+        for ($i=0; $i<$count;$i=$i+4) {
+          echo '<tr class="snip"><td class="num">',$docnum,'.',$mark,'</td>',Teipot::snip($row['text'], $offsets[$i+2], $offsets[$i+3], $pot->baseHref.$row['href'].'?q='.$q.'#mark'.$mark),"</tr>\n";
+          $mark++;
+        }
+        $docnum++;
+        echo $more;
+      }
+    }
+    echo "\n</table>";
+  }
+  
+  /**
+   * Snip a sentence in plain-text according to a byte offset
+   * Dependant of a formated text with one sentence by line
+   */
+  static function snip ($text, $offset, $size, $href) {
+    // largeur max
+    $width=300;
+    $snip=array();
+    $snip[]='<td align="right">';
+    $start=$offset-$width;
+    $length=$width;
+    if($start < 0) {
+      $start=0;
+      $length=$offset-1;
+    }
+    if ($length) {
+      $left=substr($text, $start, $length);
+      // couper au dernier saut de ligne 
+      if ($pos=strrpos($left, "\n")) $left=substr($left, $pos);
+      // sinon couper au premier espace
+      else if ($pos=strpos($left, ' ')) $left=substr($left, $pos-1);
+      $snip[]=$left;
+    }
+    $snip[]='</td><td class="mark"><a href="';
+    $snip[]=$href;
+    $snip[]='">';
+    $snip[]=substr($text, $offset, $size);
+    $snip[]="</a></td><td>";
+    $start=$offset+$size;
+    $length=$width;
+    $len=strlen($text);
+    if ($start + $length - 1 > $len) $length=$len-$start;
+    if($length) {
+      $right=substr($text, $start, $length);
+      // couper au premier saut de ligne 
+      if ($pos=strpos($right, "\n")) $right=substr($right,0, $pos);
+      // sinon couper au dernier espace
+      else if ($pos=strrpos($right, ' ')) $left=substr($right, 0, $pos);
+      $snip[]=$right;
+    }
+    $snip[]='</td>';
+    return implode('', $snip);
+  }
+}
+?>
--- index.php
+++ index.php
@@ -1,81 +1,26 @@
 <?php
+// prendre le temps
+$time_start = microtime(true);
+// mettre le sachet SQLite dans le pot
+include ('Teipot.php');
+$pot=new Teipot(dirname(__FILE__).'/critique.sqlite', 'fr');
+// est-ce qu’une resssource statique répond ? Si non, passer à la suite
+$pot->get();
 
-
-// afficher des items d'une base TEIpub
-$sqliteFile=dirname(__FILE__).'/critique.sqlite';
-$libDir=dirname(dirname(__FILE__)).'/lib/';
-// si ça crie, adapter à la conf serveur
-$path='';
-if (isset($_SERVER['PATH_INFO'])) {
-  $path=ltrim($_SERVER['PATH_INFO'], '/');
-}
 $q=false;
 if (isset($_REQUEST['q']) && $_REQUEST['q']) $q=$_REQUEST['q'];
-
-$baseHref=str_repeat("../", substr_count($path, '/'));
-$themeHref=$baseHref.'../lib/theme/';
+$themeHref=$pot->baseHref.'theme/';
 // javascript en fin de page ?
 $js=array();
-
-$pdo=new PDO("sqlite:".$sqliteFile);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
-$branch=explode('/', $path);
+// analyse URI
+$branch=explode('/', $pot->path);
 if(count($branch)>0) $bookName=$branch[0];
 if(count($branch)>1) $artName=$branch[1];
 
-
-// téléchargement direct dans un autre format
-if (!isset($bookName));
-else if ($pos=strpos($bookName, '.epub')) {
-  $bookName=substr($bookName, 0, $pos);
-  $sql =  'SELECT epub FROM book WHERE name= '.$pdo->quote($bookName);
-  foreach  ($pdo->query($sql) as $row) {
-    header("Content-Type: application/epub+zip");
-    echo $row['epub'];
-    exit;
-  }
-  header("HTTP/1.0 404 Not Found");
-  exit;
-}
-else if ($pos=strpos($bookName, '.txt')) {
-  $bookName=substr($bookName, 0, $pos);
-  $sql =  'SELECT txt FROM book WHERE name= '.$pdo->quote($bookName);
-  foreach  ($pdo->query($sql) as $row) {
-    header("Content-Type: text/plain;charset=utf-8");
-    echo $row['txt'];
-    exit;
-  }
-  header("HTTP/1.0 404 Not Found");
-  exit;
-}
-else if ($pos=strpos($bookName, '.xml')) {
-  $bookName=substr($bookName, 0, $pos);
-  $sql =  'SELECT tei FROM book WHERE name= '.$pdo->quote($bookName);
-  foreach  ($pdo->query($sql) as $row) {
-    header("Content-Type: text/xml");
-    echo $row['tei'];
-    exit;
-  }
-  header("HTTP/1.0 404 Not Found");
-  exit;
-}
-else if ($pos=strpos($bookName, '.html')) {
-  $bookName=substr($bookName, 0, $pos);
-  $sql =  'SELECT html FROM book WHERE name= '.$pdo->quote($bookName);
-  foreach  ($pdo->query($sql) as $row) {
-    header("Content-Type: text/html;charset=utf-8");
-    echo $row['html'];
-    exit;
-  }
-  header("HTTP/1.0 404 Not Found");
-  exit;
-}
-
-
 // un livre ?
 if (isset($bookName)) {
-  $sql =  'SELECT id,nav FROM book WHERE name= '.$pdo->quote($bookName);
-  foreach  ($pdo->query($sql) as $row) {
+  $sql =  'SELECT id,nav FROM book WHERE name= '.$pot->pdo->quote($bookName);
+  foreach  ($pot->pdo->query($sql) as $row) {
     $bookId=$row['id'];
     $bookNav=$row['nav'];
     break;
@@ -86,8 +31,8 @@ if (isset($bookName)) {
   if ($artName=='index' and $q);
   else if(isset($bookId) and isset($artName)) {
     // perfs ? identifiant plus unique ?
-    $sql =  'SELECT id,body,head,breadcrumb FROM article WHERE book='.$bookId.' AND name= '.$pdo->quote($artName);
-    foreach  ($pdo->query($sql) as $row) {
+    $sql =  'SELECT id,body,head,breadcrumb FROM article WHERE book='.$bookId.' AND name= '.$pot->pdo->quote($artName);
+    foreach  ($pot->pdo->query($sql) as $row) {
       $artBody=$row['body'];
       $artHead=$row['head'];
       $artBreadcrumb=$row['breadcrumb'];
@@ -96,7 +41,7 @@ if (isset($bookName)) {
       if ($q) {
         // on croise la table des articles avec la table full-text, la table full-text n'indexe pas certaines colonnes utiles à des tris (date, auteur)
         // sur le fts3 sorbonne il faut d'abord faire le match avant de tester le rowid
-        $query=$pdo->prepare('SELECT offsets(search) AS offsets FROM search WHERE   +rowid = '.$artId.' AND text MATCH ?') ;
+        $query=$pot->pdo->prepare('SELECT offsets(search) AS offsets FROM search WHERE   +rowid = '.$artId.' AND text MATCH ?') ;
         $query->execute(array($q ));
         // TODO, join quote expressions
         $mark=1;
@@ -145,7 +90,7 @@ if (isset($bookName)) {
   <body class="<?php if($q) echo 'search'; ?>">
     <header id="header">
       <h1>
-        <a href="<?php echo $baseHref; ?>">OBVIL, corpus critique</a>
+        <a href="<?php echo $pot->baseHref; ?>">OBVIL, corpus critique</a>
       </h1>
       <!-- ??
       <menu class="bar">
@@ -156,21 +101,14 @@ if (isset($bookName)) {
       style="float:right; margin-right:1em; margin-top:1em;
       " >
         <input name="q" id="q" name="search" size="25" accesskey="f" tabindex="1" title="Rechercher dans le corpus [alt-shift-f]" autocomplete="off" placeholder="Rechercher"
-value="<?php echo str_replace('"', '&quot;', $q); ?>"
-style="
-    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAAQCAIAAABY/YLgAAAAJUlEQVQIHQXBsQEAAAjDoND/73UWdnerhmHVsDQZJrNWVg3Dqge6bgMe6bejNAAAAABJRU5ErkJggg==');
-    background-position: left top;
-    background-repeat: repeat-x;
-border:1px #FFFFFF solid;;
-padding:1px 1ex;
-    "/>
+value="<?php echo str_replace('"', '&quot;', $q); ?>"/>
         <button name="go">&gt;</button>
       </form>
     </header>
     <div id="center">
       <nav id="breadcrumb">
         <?php 
-        echo '<a href="',$baseHref,'">OBVIL, corpus critique</a> » '; 
+        echo '<a href="',$pot->baseHref,'">OBVIL, corpus critique</a> » '; 
         if (isset($artBreadcrumb)) echo $artBreadcrumb; 
         ?>
       </nav>
@@ -212,21 +150,7 @@ else if ($q) {
 }
 // pas de livre trouvé, proposer la liste
 else {
-  echo '
-<table class="sortable">
-  <tr><th>Auteur(s)</th><th>Date</th><th>Titre</th></tr>  
-  ';
-  $sql =  'SELECT name, byline, created, title FROM book ORDER BY byline, created';
-  foreach  ($pdo->query($sql) as $row) {
-    // '<th>','<a href="',$baseHref, $row['name'],'/">',$row['name'],'</a></th>',
-    echo '<tr>','<td>',$row['byline'],'</td>','<td>',$row['created'],'</td>','<td><a href="',$baseHref, $row['name'],'/">',$row['title'],'</a></td>',
-      '<td><a class="epub" href="',$baseHref, $row['name'],'.epub">.epub</a></td>',
-      '<td><a class="tei" href="',$baseHref, $row['name'],'.xml">.xml</a></td>',
-      '<td><a class="txt" href="',$baseHref, $row['name'],'.txt">.txt</a></td>',
-      '<td><a class="html" href="',$baseHref, $row['name'],'.html">.html</a></td>',
-      "</th>\n";
-  }
-  echo "</table>\n";
+  $pot->bib();
 }
       ?>
       </article>
@@ -247,55 +171,3 @@ if (isset($bookNav)) echo Teipot::reHref($bookNav);
     <script type="text/javascript" src="<?php echo $themeHref; ?>Sortable.js">//</script>
   </body>
 </html>
-<?php
-/** Classe provisoire en cours de mise au point pour exploiter une base SQLite produite par Teipub */
-class Teipot {
-  // réécrire certains liens dans du html pour transmettre des paramètres de requêt
-  static function reHref($html, $q=null) {
-    if(!$q && !isset($_REQUEST['q'])) return $html;
-    if(!$q) $q=$_REQUEST['q'];
-    return preg_replace('@ href="([^"]+)"@', ' href="$1?q='.$q.'#mark1"', $html);
-  }
-
-  // découper les phrases dans le texte à partir de la position du terme trouvé
-  // c'est du propre, à une phrase par ligne
-  static function snip ($text, $offset, $size, $href) {
-    // largeur max
-    $width=300;
-    $snip=array();
-    $snip[]='<tr class="snip"><td align="right">';
-    $start=$offset-$width;
-    $length=$width;
-    if($start < 0) {
-      $start=0;
-      $length=$offset-1;
-    }
-    if ($length) {
-      $left=substr($text, $start, $length);
-      // couper au dernier saut de ligne 
-      if ($pos=strrpos($left, "\n")) $left=substr($left, $pos);
-      // sinon couper au premier espace
-      else if ($pos=strpos($left, ' ')) $left=substr($left, $pos-1);
-      $snip[]=$left;
-    }
-    $snip[]='</td><td class="mark"><a href="';
-    $snip[]=$href;
-    $snip[]='">';
-    $snip[]=substr($text, $offset, $size);
-    $snip[]="</a></td><td>";
-    $start=$offset+$size;
-    $length=$width;
-    $len=strlen($text);
-    if ($start + $length - 1 > $len) $length=$len-$start;
-    if($length) {
-      $right=substr($text, $start, $length);
-      // couper au premier saut de ligne 
-      if ($pos=strpos($right, "\n")) $right=substr($right,0, $pos);
-      // sinon couper au dernier espace
-      else if ($pos=strrpos($right, ' ')) $left=substr($right, 0, $pos);
-      $snip[]=$right;
-    }
-    $snip[]='</td></tr>';
-    return implode('', $snip);
-  }
-}
