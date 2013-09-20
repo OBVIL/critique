5bb530eed5cc026633c57d67cff9dee582d6aed9
Frédéric Glorieux <glorieux-f@users.noreply.github.com>
2013-09-20 12:13:29 +0000

    install

--- index.php
+++ index.php
@@ -1,76 +1,107 @@
 <?php
-// mettre le sachet SQLite dans le pot
+// prendre le pot
 include (dirname(__FILE__).'/../lib/teipot/Teipot.php');
+// mettre le sachet SQLite dans le pot
 $pot=new Teipot(dirname(__FILE__).'/critique.sqlite', 'fr');
-// est-ce qu’une resssource statique répond ? 
-$pot->get();
+// est-ce qu’un fichier statique en base est attendu pour ce chemin ? Si oui, envoyer. 
+$pot->get($pot->path);
+// Si un document correspond à ce chemin, charger un tableau avec différents composants (body, head, breadcrumb…)
+$doc=$pot->doc($pot->path);
+// chemin css, js ; baseHref est le nombre de '../' utile pour revenir en racine du site
 $themeHref=$pot->baseHref.'../lib/teipot/';
-// get components from URI
-$divs=$pot->divs();
 
 
 ?><!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8" />
-    <?php echo $divs['head']; ?>
+    <?php echo $doc['head']; ?>
     <link rel="stylesheet" type="text/css" href="<?php echo $themeHref; ?>html.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo $themeHref; ?>teipot.css" />
   </head>
-  <body class="<?php if($pot->q) echo 'search'; ?>">
+  <body>
     <header id="header">
       <h1>
         <a href="<?php echo $pot->baseHref; ?>">OBVIL, corpus critique</a>
       </h1>
-      <!-- ??
-      <menu class="bar">
-        <li><a href="?">?</a></li>
-      </menu>
-      -->
-      <form name="q" id="qForm" action="<?php echo $pot->baseHref; ?>"
-      style="float:right; margin-right:1em; margin-top:1ex; line-height:100%;" >
-        
-        <input name="q" id="q" name="search" size="25" accesskey="f" tabindex="1" title="Rechercher dans le corpus [alt-shift-f]" autocomplete="off" placeholder="Rechercher"
-value="<?php echo str_replace('"', '&quot;', $pot->q); ?>"/>
-        <button name="go">&gt;</button>
-        <?php if (false && $bookId) echo '<br/><label>uniquement dans ce livre <input name="bookId" value="',$bookName,'" type="checkbox"/></label>'; ?>
-        
-      </form>
+      
     </header>
     <div id="center">
       <nav id="breadcrumb">
         <?php 
-        echo '<a href="',$pot->baseHref,'">OBVIL, corpus critique</a> » '; 
-        echo $divs['breadcrumb']; 
+        echo '<a href="',$pot->baseHref,'">OBVIL, corpus critique</a> » ';
+        // nous avons un livre, glisser aussi les liens de téléchargement
+        echo $doc['breadcrumb']; 
         ?>
       </nav>
       <article id="article">
       <?php
 
-if ($divs['body']) {
-  echo $divs['body'];
-  // page d’accueil d’un livre et requête, afficher une concordance dans le livre
-  if ($pot->q && (!$divs['artName'] || $divs['artName']=='index')) echo $pot->concBook($divs['bookId']);
+if ($doc['body']) {
+  echo $doc['body'];
+  // page d’accueil d’un livre avec recherche plein texte, afficher une concordance
+  if ($pot->q && (!$doc['artName'] || $doc['artName']=='index')) echo $pot->concBook($doc['bookId']);
 }
-// pas de livre trouvé, proposer la liste
+// pas de livre demandé, montrer un rapport général
 else {
-  $pot->bib();
+  // charger des résultats en mémoire
+  $pot->search();
+  // nombre de résultats
+  echo $pot->report();
+  // présentation chronologique des résultats
+  echo $pot->chrono();
+  // présentation bibliographique des résultats
+  echo $pot->biblio();
+  // concordance s’il y a recherche plein texte
+  echo $pot->conc();
 }
       ?>
       </article>
       <aside id="aside">
-        <menu>
-<?php
-// ajouter la recherche de mots aux sous parties
-echo $pot->reHref($divs['nav']);
+        <p> </p>
+          <?php
+// livre
+if ($doc['bookId']) {
+  echo "\n<nav>";
+  // liens de téléchargements
+  echo "\n".'<div class="downloads"><small>Télécharger :</small> '.$doc['downloads'].'</div>';
+  // auteur, titre, date
+  if ($doc['byline']) $doc['byline']=$doc['byline'].'<br/>';
+  echo "\n".'<header><a href="'.$pot->baseHref.$doc['bookName'].'/">'.$doc['byline'].$doc['title'].' ('.$doc['end'].')</a></header>';
+  // rechercher dans ce livre
+  echo '
+  <form>
+    <small>Rechercher dans ce livre</small><br/>
+    <input name="q" id="q" onclick="this.select()" name="search" size="25" title="Rechercher dans ce livre" value="'. str_replace('"', '&quot;', $pot->q) .'"/>
+    <button name="go">&gt;</button>
+  </form>
+  ';
+  // table des matières
+  echo $doc['toc'];
+  echo "\n</nav>";
+}
+// accueil ? formulaire de recherche général
+else {
+  echo'
+    <h1>Rechercher dans la collection</h1>
+    <form action="">
+      <input name="q" class="text" value="'.str_replace('"', '&quot;', $pot->q).'"/>
+      <div><label>Dates,</label> de <input name="start" class="year" value="'.$pot->start.'"/>à <input class="year" name="end" value="'. $pot->end .'"/></div>
+      <div><label>Auteurs :</label>'.$pot->byList().'</div>
+      <button type="reset" onclick="return Form.reset(this.form)">Effacer</button>
+      <button type="submit">Rechercher</button>
+    </form>
+  ';
+}
 ?>
-        </menu>
       </aside>
     </div>
     <footer id="footer">
       Prototype d'application TEI pour le corpus critique
     </footer>
     <script type="text/javascript" src="<?php echo $themeHref; ?>Tree.js">//</script>
+    <script type="text/javascript" src="<?php echo $themeHref; ?>Form.js">//</script>
     <script type="text/javascript" src="<?php echo $themeHref; ?>Sortable.js">//</script>
-    <script type="text/javascript"><?php echo $divs['js']; ?></script>  
+    <script type="text/javascript"><?php echo $doc['js']; ?></script>  
   </body>
 </html>
